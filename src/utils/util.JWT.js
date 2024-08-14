87c257274afff89975526230a2156e9cec0436e0
Tohir Ravshanov <tohirdavrsmart@gmail.com>
2024-08-14 16:49:20 +0500

    Update util.JWT.js

--- src/utils/util.JWT.js
+++ src/utils/util.JWT.js
@@ -2,7 +2,7 @@ import JWT from 'jsonwebtoken'
 console.log(process.env.JWT_SECRET);
 export default {
     sign: payload => JWT.sign(payload, process.env.JWT_SECRET_KEY, {
-        expiresIn: "5h"
+        expiresIn: "5d"
     }),
     verify: token => JWT.verify(token, process.env.JWT_SECRET_KEY, {
         expiresIn: "5h"
